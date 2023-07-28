9791268a4943aeeecc9e7f468ee9257f93db85be
Jonnathan Valdez <jonathanandresvaldezllivisaca@gmail.com>
2023-07-28 17:03:27 -0500

    First Commit

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,12 @@
+import { IsString, IsPositive, MinLength, IsInt, Min} from "class-validator";
+
+export class CreatePokemonDto {
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no: number;
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+}
